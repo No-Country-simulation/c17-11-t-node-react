62372b90db29a48b626898a3c7f772a44b486bc5
AgusDela <dagustin001@gmail.com>
2024-04-12 20:11:36 -0300

    feat: :construction: new constructor, in progress!

--- back/src/service/service.module.ts
+++ back/src/service/service.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Service, ServiceSchema } from './schemas/service.schema';
+import { ServiceService } from './service.service';
+import { ServicesController } from './v1/services/services.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Service.name, schema: ServiceSchema }]),
   ],
+  providers: [ServiceService],
+  controllers: [ServicesController],
 })
 export class ServiceModule {}
---
+++ back/src/service/service.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+
+///LE pega a bdd conexion con el esquema
+// Empiezo aca
+
+export class ServiceService {
+    constructor(
+        
+    )
+}
