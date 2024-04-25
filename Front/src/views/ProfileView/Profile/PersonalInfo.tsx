ff5a374dd4769c899d791861f66e841878e3b31e
nacho <jose-imhoff@hotmail.com>
2024-04-24 22:46:10 -0300

    Add changes to profile page

--- Front/src/views/ProfileView/Profile/PersonalInfo.tsx
+++ Front/src/views/ProfileView/Profile/PersonalInfo.tsx
@@ -2,8 +2,8 @@ export const PersonalInfo=()=>{
     return(
         <div className="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
             <div className="w-full flex flex-col 2xl:w-1/3">
-                <div className="flex-1 bg-white rounded-lg shadow-xl p-8">
-                    <h4 className="text-xl text-gray-900 font-bold">Personal Info</h4>
+                <div className="flex-1 bg-white rounded-lg shadow-xl p-8 max-sm:text-sm">
+                    <h4 className="text-center text-xl text-gray-900 font-bold">Personal Info</h4>
                     <ul className="mt-2 text-gray-700">
                         <li className="flex border-y py-2">
                             <span className="font-bold w-24">Full name:</span>
@@ -28,10 +28,6 @@ export const PersonalInfo=()=>{
                         <li className="flex border-b py-2">
                             <span className="font-bold w-24">Location:</span>
                             <span className="text-gray-700">New York, US</span>
-                        </li>
-                        <li className="flex border-b py-2">
-                            <span className="font-bold w-24">Languages:</span>
-                            <span className="text-gray-700">English, Spanish</span>
                         </li>
                     </ul>
                 </div>
---
+++ Front/src/views/ProfileView/Profile/Review.tsx
@@ -0,0 +1,16 @@
+export const Review = () => {
+    return (
+        <div className="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
+            <div className="w-full flex flex-col 2xl:w-1/3">
+                <div className="flex-1 bg-white rounded-lg shadow-xl p-8 text-lg max-sm:text-sm">
+                    <h4 className="text-center text-xl text-gray-900 font-bold">Review</h4>
+                    <h5 className="font-bold">Irene</h5>
+                    <p className="text-gray-700">Amanda took good care of my little dog. She kept us up to date on her walks, activities and moods. We felt very calm during the week she stayed with her. She was patient with my dog with everything and nights. He was kind enough to wash the bed.</p>
+                    <h4 className="font-bold">Sofia</h4>
+                    <p className="text-gray-700">My dog Missy was very well cared for and pampered by Aranza and her grandmother. It is seen that Mrs. Alejandra has experience in pet care. I wouldn’t hesitate to leave Missy with them again. Thank you for taking care of her as well as I would</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
